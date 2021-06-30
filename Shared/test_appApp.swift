7e55eeeb07ef415e06a1ae9c2ebed3f669a6d0d7
max <harumaxy@gmail.com>
2021-06-30 22:27:57 +0900

    Initial Commit

---
+++ Shared/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  Shared
+//
+//  Created by Masaharu Hosomichi on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
